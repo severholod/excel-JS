69cae4b700d616bd6220e5f2dd058a04e2e3de03
Sever <karapuchelo98@gmail.com>
2020-09-14 22:06:48 +0400

    DIP implemented

---
+++ src/core/page/StateProcessor.js
@@ -0,0 +1,15 @@
+import {debounce} from '@core/utils';
+
+export class StateProcessor {
+    constructor(client, delay = 500) {
+        this.listen = debounce(this.listen.bind(this), delay)
+        this.client = client
+    }
+
+    listen(state) {
+        this.client.save(state)
+    }
+    get() {
+        return this.client.get()
+    }
+}
